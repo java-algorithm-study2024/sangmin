26c52df38c25ef4f4767838aa74d476e8e7337cb
이상민 <dkwkghkdlxld2@gmail.com>
2024-06-03 00:11:11 +0900

    Update 같은 숫자는 싫어(프로그래머스).java

--- 스택/같은 숫자는 싫어(프로그래머스).java
+++ 스택/같은 숫자는 싫어(프로그래머스).java
@@ -2,28 +2,26 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        
+    public int[] solution(int[] arr) {
+
         Stack<Integer> stack = new Stack<>();
         ArrayList<Integer> array = new ArrayList();
-        
-        for(int element : arr) {
-            
-            if(stack.isEmpty()){
-                stack.push(element);
-                array.add(element);
-            } else if(!stack.isEmpty() && stack.peek() != element){
-                stack.push(element);
-                array.add(element);
+
+        for (int element : arr) {
+            if (!stack.isEmpty() && stack.peek() == element) {
+                continue;
             }
+
+            stack.push(element);
+            array.add(element);
         }
-        
+
         int[] answer = new int[array.size()];
-        for(int i=0; i<answer.length; i++){
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = array.get(i);
         }
 
-                return answer;
+        return answer;
     }
-            
+
 }
